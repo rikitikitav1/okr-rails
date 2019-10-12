674aa1dc7d7ac03f892aca9aa99fc9148efff1a8
Konstantin Nechaev <knechaev@wallarm.com>
2019-10-12 15:32:47 +0300

    chapters 1..4

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class StaticPagesController < ApplicationController
+  def initialize
+    # @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+
+  def home; end
+
+  def help; end
+
+  def about; end
+end
